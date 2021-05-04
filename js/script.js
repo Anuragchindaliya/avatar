ed4015c528c5c35e44a7375589e2a09178ab8105
AnuragChindaliya <ak1148383@gmail.com>
2021-05-04 21:40:34 +0530

    intro and responsive page

--- js/script.js
+++ js/script.js
@@ -10,14 +10,19 @@ $(document).ready(function(){
         glare: true,
         "max-glare": 1,
     });
+    
 
 
     $('button').click(function(e){
         e.preventDefault();
-        const v = $('#s').val();
+        const v = $('#s').val().trim();
+        if(v.length==0){
+            $(".msg").text(`Don't be oversmart, please fill your name`);
+        }else{
         $(".avatar").attr("src",`https://joeschmoe.io/api/v1/${v}`);
         $("#s").trigger('reset');
-        $(".msg").text(`Hii ${v}`)
+        $(".msg").text(`Hii ${v}, Really is this you`);
+        }   
         // $(".avatar-container").append(`<a href="https://joeschmoe.io/api/v1/${v}">Download</a>`);
     });
 
